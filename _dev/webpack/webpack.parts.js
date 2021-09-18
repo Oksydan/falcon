f436fbaa1902f4d1abbfa46efe871e5db26f1476
Oksydan <oksydan56@gmail.com>
2021-09-18 21:02:38 +0200

    removed enhanced-webpack-chunk-rename-plugin from webpack config

--- _dev/webpack/webpack.parts.js
+++ _dev/webpack/webpack.parts.js
@@ -1,6 +1,5 @@
 const chokidar = require('chokidar');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ChunkRenamePlugin = require('enhanced-webpack-chunk-rename-plugin');
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
