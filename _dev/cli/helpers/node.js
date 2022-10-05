062718946948cd799798a390362d6553861e8fec
Igor Stępień <igor@istepien.dev>
2022-10-05 23:10:52 +0200

    CLI tool moved to separated package

--- _dev/cli/helpers/node.js
+++
@@ -1,9 +0,0 @@
-const semver = require('semver');
-const execa = require('execa');
-const which = require('which');
-
-exports.nodeModuleChecker = (supportedNodeVersion) => semver.satisfies(process.version, supportedNodeVersion, { includePrerelease: true });
-
-exports.detectYarn = () => which.sync('yarn', { nothrow: true });
-
-exports.detectPnpm = () => which.sync('pnpm', { nothrow: true });
