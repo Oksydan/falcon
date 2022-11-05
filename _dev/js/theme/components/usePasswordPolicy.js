0e2c63c0a6e88784db02f3c4f5fd04b4a6298c9f
Igor Stępień <igor@istepien.dev>
2022-11-05 23:59:58 +0100

    Privacy policy adjustments to recent changes in 8.0.0 rc

--- _dev/js/theme/components/usePasswordPolicy.js
+++ _dev/js/theme/components/usePasswordPolicy.js
@@ -38,7 +38,7 @@ const getPasswordStrengthFeedback = (
   }
 };
 
-const watchPassword = (
+const watchPassword = async (
   elementInput,
   feedbackContainer,
   hints,
@@ -46,7 +46,7 @@ const watchPassword = (
   const { prestashop } = window;
   const passwordValue = elementInput.value;
   const elementIcon = feedbackContainer.querySelector(PasswordPolicyMap.requirementScoreIcon);
-  const result = prestashop.checkPasswordScore(passwordValue);
+  const result = await prestashop.checkPasswordScore(passwordValue);
   const feedback = getPasswordStrengthFeedback(result.score);
   const passwordLength = passwordValue.length;
   const popoverContent = [];
