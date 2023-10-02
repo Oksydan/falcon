095112bc5d0c4f23c3a71e419949c79ffa1f14bf
Igor Stępień <igor@istepien.dev>
2023-10-02 02:48:11 +0200

    Add to cart fix

--- _dev/js/theme/core/product/index.js
+++ _dev/js/theme/core/product/index.js
@@ -1,10 +1,8 @@
-import updateProduct from '@js/theme/core/product/updateProduct';
 import productController from './productController';
 import DOMReady from '../../utils/DOMReady';
 
 const { init } = productController();
 
 DOMReady(() => {
-  updateProduct();
   init();
 });
