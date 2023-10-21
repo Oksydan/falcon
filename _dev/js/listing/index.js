068c386562ecbb56695ddd49241784de401a7888
Igor Stępień <igor@istepien.dev>
2023-10-21 02:45:59 +0200

    listing jquery removed

--- _dev/js/listing/index.js
+++ _dev/js/listing/index.js
@@ -1,38 +1,36 @@
-import $ from 'jquery';
 import prestashop from 'prestashop';
 import Filters from '@js/listing/components/filters/Filters';
+import parseToHtml from '../theme/utils/parseToHtml';
+import { each } from '../theme/utils/DOMHelpers';
+import DOMReady from '../theme/utils/DOMReady';
 
 function updateProductListDOM(data) {
-  $(prestashop.selectors.listing.searchFilters).replaceWith(
-    data.rendered_facets,
-  );
-  $(prestashop.selectors.listing.activeSearchFilters).replaceWith(
-    data.rendered_active_filters,
-  );
-  $(prestashop.selectors.listing.listTop).replaceWith(
-    data.rendered_products_top,
-  );
-
-  const renderedProducts = $(data.rendered_products);
-  const productSelectors = $(prestashop.selectors.listing.product);
-
-  if (productSelectors.length > 0) {
-    productSelectors.removeClass().addClass(productSelectors.first().attr('class'));
-  } else {
-    productSelectors.removeClass().addClass(renderedProducts.first().attr('class'));
-  }
-
-  $(prestashop.selectors.listing.list).replaceWith(renderedProducts);
-  $(prestashop.selectors.listing.listBottom).replaceWith(data.rendered_products_bottom);
+  each(prestashop.selectors.listing.searchFilters, (el) => {
+    el.replaceWith(parseToHtml(data.rendered_facets));
+  });
+  each(prestashop.selectors.listing.activeSearchFilters, (el) => {
+    el.replaceWith(parseToHtml(data.rendered_active_filters));
+  });
+  each(prestashop.selectors.listing.listTop, (el) => {
+    el.replaceWith(parseToHtml(data.rendered_products_top));
+  });
+  each(prestashop.selectors.listing.listBottom, (el) => {
+    el.replaceWith(parseToHtml(data.rendered_products_bottom));
+  });
+  each(prestashop.selectors.listing.list, (el) => {
+    el.replaceWith(parseToHtml(data.rendered_products));
+  });
 
   if (data.rendered_products_header) {
-    $(prestashop.selectors.listing.listHeader).replaceWith(data.rendered_products_header);
+    each(prestashop.selectors.listing.listHeader, (el) => {
+      el.replaceWith(parseToHtml(data.rendered_products_header));
+    });
   }
 
   prestashop.emit('updatedProductList', data);
 }
 
-$(() => {
+DOMReady(() => {
   /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "filters" }] */
   const filters = new Filters();
 
