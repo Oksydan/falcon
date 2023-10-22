1eb4464254c8db572c6f6add9c2fe63de18c7f4f
Igor Stępień <igor@istepien.dev>
2023-10-23 01:07:22 +0200

    utils code jsdocs

--- _dev/js/theme/core/email/emailIdn.js
+++ _dev/js/theme/core/email/emailIdn.js
@@ -1,20 +1,32 @@
 import punycode from 'punycode';
 
+/**
+ * Initializes the conversion of Internationalized Domain Names (IDN) in email fields.
+ * @param {string} selector - The CSS selector for the email fields.
+ */
 const initEmailIdn = (selector) => {
-  const emailFields = document.querySelectorAll(selector);
+  /**
+   * Handles the conversion of IDN in the provided email field.
+   * @param {HTMLInputElement} field - The email input field.
+   */
   const handleField = (field) => {
+    // Check if the email field is not valid
     if (!field.checkValidity()) {
       const parts = field.value.split('@');
 
+      // Convert the local part of the email address to ASCII if needed
       if (punycode.toASCII(parts[0]) === parts[0]) {
         field.value = punycode.toASCII(field.value);
       }
     }
   };
 
-  emailFields.forEach((field) => {
+  // Iterate through each email field
+  document.querySelectorAll(selector).forEach((field) => {
+    // Initial handling of the email field
     handleField(field);
 
+    // Attach blur event listener to re-handle the email field on blur
     field.addEventListener('blur', () => {
       handleField(field);
     });
@@ -22,3 +34,4 @@ const initEmailIdn = (selector) => {
 };
 
 export default initEmailIdn;
+
