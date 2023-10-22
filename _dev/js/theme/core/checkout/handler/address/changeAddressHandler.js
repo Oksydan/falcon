25b67db80ce3d22403a2bb901e8c196742569419
Igor Stępień <igor@istepien.dev>
2023-10-23 00:42:44 +0200

    minor jsdocs changes to checkout

--- _dev/js/theme/core/checkout/handler/address/changeAddressHandler.js
+++ _dev/js/theme/core/checkout/handler/address/changeAddressHandler.js
@@ -6,8 +6,10 @@ import switchConfirmAddressesButtonState from '../../utils/switchConfirmAddresse
 import { isElementVisible, each } from '../../../../utils/DOMHelpers';
 
 /**
- * Change address handler
- * @param event {object} - change event
+ * Handles the change event for address selection in the Prestashop checkout process.
+ *
+ * @function
+ * @param {object} event - The change event object.
  */
 const changeAddressHandler = (event) => {
   const {
