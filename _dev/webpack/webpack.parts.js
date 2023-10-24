dba0abca49d0cc31115b297e2d7861c65892edc6
Igor Stępień <igor@istepien.dev>
2023-10-24 02:13:50 +0200

    Removed not used chokidar

--- _dev/webpack/webpack.parts.js
+++ _dev/webpack/webpack.parts.js
@@ -1,4 +1,3 @@
-const chokidar = require('chokidar');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
