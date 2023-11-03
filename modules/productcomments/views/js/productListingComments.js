d506701f76a6b56f081d804252cd01b11c4e8644
Igor Stępień <igor@istepien.dev>
2023-11-03 13:01:07 +0100

    product comments listing stars fix

--- modules/productcomments/views/js/productListingComments.js
+++ modules/productcomments/views/js/productListingComments.js
@@ -37,7 +37,7 @@ const productListingComments = (() => {
         if (data.ajaxUrl !== '')
             return;
 
-        data.ajaxUrl = document.querySelector(DOMStrings.productListReviewsContainer).dataset.url;
+        data.ajaxUrl = document.querySelector(DOMStrings.productListReviewsContainer)?.dataset?.url;
     }
 
     const getNewProductsReviewsElements = () => {
